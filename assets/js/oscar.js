7d18749809820006e65371002c05fdfee06ca4e4
Martin Kausche <mail@martinkausche.de>
2017-02-05 20:17:08 +0100

    modified header (always small, always same layout, no js-stuff anymore) modified header of post (right-aligned, same as index)

--- assets/js/oscar.js
+++ assets/js/oscar.js
@@ -1,9 +1,11 @@
 /* toggle menu in home and tag template */
+/*
 $('.home-template .masthead, .tag-template .masthead').mouseenter(function(){
 	$('.nav').slideToggle(200);
 }).mouseleave(function(){
 	$('.nav').slideToggle(200);
 });
+*/
 
 $('a[href^=http]').each(function(){
     if(this.href.indexOf(location.hostname) < 0) {
